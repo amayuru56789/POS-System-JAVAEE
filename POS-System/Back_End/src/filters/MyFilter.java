412ed11b212d852f7aa8c823f8daaf0a21b9558d
amayuru56789 <indeewara5678@gmail.com>
2022-06-14 00:30:23 +0530

    added Filters for CORS policy Error in Pos_System

---
+++ POS-System/Back_End/src/filters/MyFilter.java
@@ -0,0 +1,33 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/customer")
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        /*cast servletRequest & servletResponse for sub class*/
+        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        HttpServletResponse resp = (HttpServletResponse)servletResponse;
+
+        resp.addHeader("Access-Control-Allow-Origin","*"); //added header for CORS Policy issue
+
+        filterChain.doFilter(servletRequest,servletResponse); //request proceed to the servlet
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
